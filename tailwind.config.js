b334917603091122fe7a7228d60b553a08bcec98
AD-iT448 <arihades.123@gmail.com>
2024-07-18 20:48:51 +0800

    fixing AppVideoBelajar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'lime' : '#a3e635',
+    },
+    extend: {
+      container:{
+        center:true,
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
